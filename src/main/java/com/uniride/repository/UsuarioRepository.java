6ae6d6a0b907da8113f77007cf4d6a7fa87e575f
Marcos Aaron  Bedia Torres <u202118582@upc.edu.pe>
2025-09-29 07:23:43 -0500

    feat(validacion-usuario): Implemente validacion de cuenta de usuario por correo y dni.

--- src/main/java/com/uniride/repository/UsuarioRepository.java
+++ src/main/java/com/uniride/repository/UsuarioRepository.java
@@ -2,8 +2,8 @@ package com.uniride.repository;
 
 import com.uniride.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
