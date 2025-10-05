828cd73bde7fd64062080014352b195a3cc532f5
NArmando16 <103780431+NArmando16@users.noreply.github.com>
2025-10-05 16:40:04 -0500

    Update-responde dtos

---
+++ src/main/java/com/uniride/dto/response/ViajeResponseDTO.java
@@ -0,0 +1,24 @@
+package com.uniride.dto.response;
+
+import com.uniride.model.enums.DiaSemana;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Set;
+@Builder
+public record ViajeResponseDTO(
+        Long idViaje,
+        Long idConductor,
+        String nombreConductor,
+        String origen,
+        String destino,
+        LocalDate fecha,
+        LocalTime hora,
+        Integer asientosDisponibles,
+        Boolean recurrente,
+        LocalDate fechaFinRecurrencia,
+        Set<DiaSemana> diasRecurrencia
+) {}
+
+
