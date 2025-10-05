07ab58f2c77ecbea564e1df97f6fedca47b124d5
NArmando16 <103780431+NArmando16@users.noreply.github.com>
2025-10-05 16:35:56 -0500

    Delete ViajeRepository.java

--- ViajeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.uniride.repository;
-
-import com.uniride.model.Viaje;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ViajeRepository extends JpaRepository<Viaje, Long> {}
